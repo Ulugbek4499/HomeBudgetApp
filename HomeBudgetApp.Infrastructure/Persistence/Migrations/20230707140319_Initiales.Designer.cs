a1e45ab520f63d101d70ba0abcbb16ae8c7c5f66
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-07 19:03:52 +0500

    save

--- HomeBudgetApp.Infrastructure/Persistence/Migrations/20230707140319_Initiales.Designer.cs
+++ HomeBudgetApp.Infrastructure/Persistence/Migrations/20230707140319_Initiales.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HomeBudgetApp.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HomeBudgetApp.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230707140319_Initiales")]
+    partial class Initiales
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HomeBudgetApp.Infrastructure/Persistence/Migrations/20230707140319_Initiales.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeBudgetApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initiales : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Time",
+                table: "Incomes",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Time",
+                table: "Expenses",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Incomes");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Expenses");
+        }
+    }
+}
