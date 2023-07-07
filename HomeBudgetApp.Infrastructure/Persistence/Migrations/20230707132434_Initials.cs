0b92914811c276ede160bcf064c3bc03b4dd79dd
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-07 18:25:13 +0500

    saved

---
+++ HomeBudgetApp.Infrastructure/Persistence/Migrations/20230707132434_Initials.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeBudgetApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    ExpenseCategory = table.Column<int>(type: "integer", nullable: false),
+                    Comment = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Incomes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    IncomeCategory = table.Column<int>(type: "integer", nullable: false),
+                    Comment = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incomes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+
+            migrationBuilder.DropTable(
+                name: "Incomes");
+        }
+    }
+}
--- HomeBudgetApp.MVC.UI/Controllers/IncomeController.cs
+++ HomeBudgetApp.MVC.UI/Controllers/IncomeController.cs
@@ -1,6 +1,65 @@
-﻿namespace HomeBudgetApp.MVC.UI.Controllers
+﻿using HomeBudgetApp.Application.Commons.Models;
+using HomeBudgetApp.Application.UseCases.Incomes.Commands.CreateIncomes;
+using HomeBudgetApp.Application.UseCases.Incomes.Commands.UpdateIncomes;
+using HomeBudgetApp.Application.UseCases.Incomes.Queries.GetIncome;
+using HomeBudgetApp.Application.UseCases.Incomes.Queries.GetIncomes;
+using Microsoft.AspNetCore.Mvc;
+using HomeBudgetApp.Application.UseCases.Incomes.Commands.DeleteIncomes;
+
+namespace HomeBudgetApp.MVC.UI.Controllers
 {
-    public class IncomeController
+    public class IncomeController : ApiBaseController
     {
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> CreateIncome()
+        {
+            return View();
+        }
+
+        [HttpPost("[action]")]
+        public async ValueTask<IActionResult> CreateIncome([FromForm] CreateIncomeCommand Income)
+        {
+            await Mediator.Send(Income);
+
+            return RedirectToAction("GetAllIncomes");
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> UpdateIncome(Guid Id)
+        {
+            var Income = await Mediator.Send(new GetIncomeQuery(Id));
+
+            return View(Income);
+        }
+
+        [HttpPost("[action]")]
+        public async ValueTask<IActionResult> UpdateIncome([FromForm] UpdateIncomeCommand Income)
+        {
+            await Mediator.Send(Income);
+            return RedirectToAction("GetAllIncomes");
+        }
+
+        public async ValueTask<IActionResult> DeleteIncome(Guid Id)
+        {
+            await Mediator.Send(new DeleteIncomeCommand(Id));
+
+            return RedirectToAction("GetAllIncomes");
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> GetAllIncomes()
+        {
+            IncomeDto[] Incomes = await Mediator.Send(new GetIncomesQuery());
+
+            return View(Incomes);
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> ViewIncome(Guid id)
+        {
+            var Income = await Mediator.Send(new GetIncomeQuery(id));
+
+            return View("ViewIncome", Income);
+        }
     }
 }
