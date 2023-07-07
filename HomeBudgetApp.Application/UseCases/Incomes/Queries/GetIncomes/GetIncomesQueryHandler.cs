2741cbf9412b269205c9550ae17f74339c9e7100
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-07 17:22:19 +0500

    save

---
+++ HomeBudgetApp.Application/UseCases/Incomes/Queries/GetIncomes/GetIncomesQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBudgetApp.Application.UseCases.Incomes.Queries.GetIncomes
+{
+    internal class GetIncomesQueryHandler
+    {
+    }
+}
